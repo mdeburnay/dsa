79fa545cd31f03bbf2f0355efb1f3a68f355674d
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2025-11-17 08:41:17 +1100

    add skeleton for doubly linked list and linked list

---
+++ DoublyLinkedList/DoublyLinkedList.ts
@@ -0,0 +1,7 @@
+type Node<T> = {
+	value: T;
+	next?: Node<T>;
+	prev?: Node<T>;
+};
+
+export class DoublyLinkedList {}
---
+++ LinkedList/LinkedList.ts
@@ -0,0 +1,6 @@
+type Node<T> = {
+	value: T;
+	next?: Node<T>;
+};
+
+export class LinkedList {}
